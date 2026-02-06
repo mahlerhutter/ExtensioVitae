7ea3c11fe6e78ebf34ef5b4df834f9188b44fba7
Manuel Mahler-Hutter <manuelmahlerhutter@gmail.com>
2026-02-06 14:08:00 +0100

    chore(test): Increase webServer timeout for Playwright

--- playwright.config.js
+++ playwright.config.js
@@ -46,5 +46,6 @@ export default defineConfig({
         command: 'npm run dev',
         url: 'http://localhost:5173',
         reuseExistingServer: !process.env.CI,
+        timeout: 120 * 1000,
     },
 });
