14af6061047a32dabfe674f8cbca6aa9aa29986a
Manuel Mahler-Hutter <manuelmahlerhutter@gmail.com>
2026-02-06 14:14:39 +0100

    chore(test): Update Playwright port to 3100

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:5173',
+        baseURL: 'http://localhost:3100',
 
         /* Collect trace when retrying the failed test. */
         trace: 'on-first-retry',
@@ -44,7 +44,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: 'http://localhost:5173',
+        url: 'http://localhost:3100',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000,
     },
